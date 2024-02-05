7df03a4879a9133467f97d30a9974fb89c750781
Aleksandr232 <aleksrowing232@gmail.com>
2024-02-06 02:04:53 +0300

    day9

--- T09D15-1-master/src/data_libs/data_io.c
+++ T09D15-1-master/src/data_libs/data_io.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 #include "data_io.h"
+
+
+void input(double *data, int n) {
+    int valid = 1;
+    if (n >= 1) {
+        for (int i = 0; i < n; i++) {
+            if (scanf("%lf ", &data[i]) != 1) {
+                valid = 0;
+                break;
+            }
+        }
+    } else {
+        valid = 0;
+    }
+
+    if (!valid) {
+        printf("n/a \n");
+    }
+}
+
+void output(double *data, int n){
+    for (int i = 0; i < n; i++) {
+        printf("%lf ", data[i]);
+    }
+}
--- T09D15-1-master/src/data_module/data_module_entry.c
+++ T09D15-1-master/src/data_module/data_module_entry.c
@@ -1,14 +1,33 @@
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+#include "data_process.h"
+#include "../data_libs/data_io.h"
+#include "../data_libs/data_stat.h"
+
+int main()
 {
     double *data;
     int n;
     
-    //Don`t forget to allocate memory !
+   
+    scanf("%d", &n);
+
+    // Выделение памяти для массива "data"
+    data = (double *)malloc(n * sizeof(double));
+
+    
 
     input(data, n);
 
     if (normalization(data, n))
         output(data, n);
-    else
+    else{
         printf("ERROR");  
+    }
+    
+
+    // Освобождение выделенной памяти
+    free(data);
+    
+    return 0;
 }
